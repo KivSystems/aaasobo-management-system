841130b8ebeeaef102a544711a52c93b7acc3f3b
Shingo023 <s22sugi@gmail.com>
2025-01-28 06:21:31 -0800

    Use reusable LoginForm component in instructors login page.

--- frontend/src/app/customers/login/page.tsx
+++ frontend/src/app/customers/login/page.tsx
@@ -14,6 +14,7 @@ export default function LoginPage() {
           width={100}
           height={100}
           className={styles.logo}
+          priority={true}
         />
         <h2>Login</h2>
         <p>
