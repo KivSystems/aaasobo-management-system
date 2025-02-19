93a7d4dd5b4d79a7e57511f175123c62044ee86d
Shingo023 <s22sugi@gmail.com>
2025-02-19 08:11:14 -0800

    feat (backend): add verification token service for email confirmation

---
+++ backend/src/services/verificationTokenService.ts
@@ -0,0 +1,59 @@
+import { v4 as uuidv4 } from "uuid";
+import { prisma } from "../../prisma/prismaClient";
+
+export const generateVerificationToken = async (email: string) => {
+  const token = uuidv4();
+  const expires = new Date(new Date().getTime() + 60 * 60 * 1000);
+
+  try {
+    // Delete an existing token to make only the latest one valid
+    const existingToken = await getVerificationTokenByEmail(email);
+
+    if (existingToken) {
+      await prisma.verificationToken.delete({
+        where: {
+          id: existingToken.id,
+        },
+      });
+    }
+
+    const verificationToken = await prisma.verificationToken.create({
+      data: {
+        email,
+        token,
+        expires,
+      },
+    });
+
+    return verificationToken;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to create a verification token.");
+  }
+};
+
+export const getVerificationTokenByToken = async (token: string) => {
+  try {
+    const verificationToken = await prisma.verificationToken.findUnique({
+      where: { token },
+    });
+
+    return verificationToken;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to get a verification token by token.");
+  }
+};
+
+export const getVerificationTokenByEmail = async (email: string) => {
+  try {
+    const verificationToken = await prisma.verificationToken.findFirst({
+      where: { email },
+    });
+
+    return verificationToken;
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to get a verification token by email.");
+  }
+};
