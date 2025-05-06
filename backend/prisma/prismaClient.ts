345241e56c148c960d6179e5f1b675f1be23bdee
mtkksk1780 <mtkksk1780@gmail.com>
2025-05-06 11:19:16 +0700

    Extend Prisma timeout

--- backend/prisma/prismaClient.ts
+++ backend/prisma/prismaClient.ts
@@ -4,6 +4,6 @@ export const prisma = new PrismaClient({
   transactionOptions: {
     isolationLevel: Prisma.TransactionIsolationLevel.Serializable,
     maxWait: 5000, // default: 2000
-    timeout: 10000, // default: 5000
+    timeout: 60000, // default: 5000
   },
 });
