6d6bf7e767def2132576d9954924b6f174325d25
mtkksk1780 <mtkksk1780@gmail.com>
2025-07-03 01:33:06 +0700

    refactor: remove unnecessary code

--- backend/src/controllers/adminsController.ts
+++ backend/src/controllers/adminsController.ts
@@ -1,4 +1,4 @@
-import e, { Request, Response } from "express";
+import { Request, Response } from "express";
 import { registerAdmin, getAdminByEmail } from "../services/adminsService";
 import {
   getAllAdmins,
