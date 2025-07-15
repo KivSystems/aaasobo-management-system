6ce172149c28b05178deb87592d04cde3c198bdc
mtkksk1780 <mtkksk1780@gmail.com>
2025-07-15 14:22:15 +0700

    feat(frontend): add design for modal

--- frontend/src/app/components/admins-dashboard/BusinessCalendarModal.tsx
+++ frontend/src/app/components/admins-dashboard/BusinessCalendarModal.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import styles from "./BusinessCalendarModal.module.scss";
 import InputField from "@/app/components/elements/inputField/InputField";
 import FormValidationMessage from "@/app/components/elements/formValidationMessage/FormValidationMessage";
 import ActionButton from "@/app/components/elements/buttons/actionButton/ActionButton";
-import { CheckIcon } from "@heroicons/react/24/outline";
+import {
+  CheckIcon,
+  CalendarDaysIcon,
+  BellIcon,
+} from "@heroicons/react/24/outline";
 
 const BusinessCalendarModal = ({
   selectedDates,
@@ -15,40 +20,49 @@ const BusinessCalendarModal = ({
   localMessages: UpdateFormState;
 }) => {
   return (
-    <>
+    <div className={styles.modalContent}>
       <h2>Schedule Update</h2>
       {selectedDates && (
         <>
-          <p>
-            {selectedDates.length === 1
-              ? selectedDates[0]
-              : `${selectedDates[0]} - ${selectedDates[1]}`}
-          </p>
           <InputField name="startDate" value={selectedDates[0]} type="hidden" />
           <InputField name="endDate" value={selectedDates[1]} type="hidden" />
-          <select name="eventId">
-            <option value="default">Select Event</option>
-            {events.map((event) => (
-              <option key={event.id} value={event.id}>
-                {event.name}
-              </option>
-            ))}
-          </select>
+          <div className={styles.dateInfo}>
+            <CalendarDaysIcon className={styles.icon} />
+            <p>
+              {selectedDates.length === 1
+                ? `${selectedDates[0]}`
+                : `${selectedDates[0]} - ${selectedDates[1]}`}
+            </p>
+          </div>
+          <div className={styles.eventSelector}>
+            <BellIcon className={styles.icon} />
+            <select name="eventId" className={styles.eventSelect}>
+              <option value="default">Select Event</option>
+              {events.map((event) => (
+                <option key={event.id} value={event.id}>
+                  {event.name}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className={styles.actionButton}>
+            <ActionButton
+              className="saveEvent"
+              btnText="Update"
+              type="submit"
+              Icon={CheckIcon}
+            />
+          </div>
           {localMessages.eventId && (
             <FormValidationMessage
               type="error"
               message={localMessages.eventId}
             />
           )}
-          <ActionButton
-            className="saveEvent"
-            btnText="Update"
-            type="submit"
-            Icon={CheckIcon}
-          />
         </>
       )}
-    </>
+    </div>
   );
 };
 
