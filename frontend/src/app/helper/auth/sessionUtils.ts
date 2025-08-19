d787977845eaa55980109fc313e291f764d23d52
mtkksk1780 <mtkksk1780@gmail.com>
2025-08-20 01:58:57 +0700

    feat(frontend): add functions to sessionUtils.ts for authentication

--- frontend/src/app/helper/auth/sessionUtils.ts
+++ frontend/src/app/helper/auth/sessionUtils.ts
@@ -1,4 +1,5 @@
 import { auth } from "../../../../auth.config";
+import { INVALID_CUSTOMER_ID } from "@/app/helper/messages/customerDashboard";
 
 export async function getUserSession(userType?: UserType) {
   const session = await auth();
@@ -12,4 +13,35 @@ export async function getUserSession(userType?: UserType) {
   }
 
   return session;
+}
+
+export async function getUserSessionType(userType: UserType, id: string) {
+  const userSession = await getUserSession(userType);
+
+  if (!userSession) {
+    return null;
+  }
+  if (
+    userType &&
+    (userSession.user.userType !== userType || userSession.user.id !== id)
+  ) {
+    return null;
+  }
+
+  return userSession.user.userType;
+}
+
+export async function authenticateUserSession(userType: UserType, id: string) {
+  const userSessionType = await getUserSessionType(userType, id);
+  if (!userSessionType && userSessionType !== userType) {
+    switch (userType) {
+      case "customer":
+        throw new Error(INVALID_CUSTOMER_ID);
+      // TODO: Add more cases as needed
+      default:
+        throw new Error("Invalid user type or id");
+    }
+  }
+
+  return userSessionType;
 }
