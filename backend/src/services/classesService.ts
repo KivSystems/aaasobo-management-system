b24f74609d9f1c7b45c7bf0006f742f095f1dd72
mtkksk1780 <mtkksk1780@gmail.com>
2025-10-15 18:00:33 +0700

    feat(backend): update getSameDateClasses in classService.ts to fetch instructors nickname

--- backend/src/services/classesService.ts
+++ backend/src/services/classesService.ts
@@ -665,6 +665,7 @@ export const getSameDateClasses = async (
       updatedAt: true,
       instructor: {
         select: {
+          nickname: true,
           classURL: true,
           meetingId: true,
           passcode: true,
@@ -746,45 +747,11 @@ export const getSameDateClasses = async (
     orderBy: { dateTime: "asc" },
   });
 
-  // If the applicable customer has already left, add "(Left)" to the children's names
-  if (targetClass.customer.terminationAt) {
-    targetClass.customer.children = targetClass.customer.children.map(
-      (child) => ({
-        ...child,
-        name: `${child.name} (Left)`,
-      }),
-    );
-    targetClass.classAttendance = targetClass.classAttendance.map(
-      (attendance) => ({
-        ...attendance,
-        children: {
-          ...attendance.children,
-          name: `${attendance.children.name} (Left)`,
-        },
-      }),
-    );
-  }
-
-  classes.forEach((cls) => {
-    if (cls.customer.terminationAt) {
-      cls.customer.children = cls.customer.children.map((child) => ({
-        ...child,
-        name: `${child.name} (Left)`,
-      }));
-      cls.classAttendance = cls.classAttendance.map((attendance) => ({
-        ...attendance,
-        children: {
-          ...attendance.children,
-          name: `${attendance.children.name} (Left)`,
-        },
-      }));
-    }
-  });
-
   const formattedTargetClass = {
     id: targetClass.id,
     dateTime: targetClass.dateTime!.toISOString(),
     customerName: targetClass.customer.name,
+    instructorName: targetClass.instructor?.nickname || "",
     classURL: targetClass.instructor?.classURL || "",
     meetingId: targetClass.instructor?.meetingId || "",
     passcode: targetClass.instructor?.passcode || "",
