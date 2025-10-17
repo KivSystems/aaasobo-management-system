af9212a203171546d1f8dba1addefe9e6e0de997
mtkksk1780 <mtkksk1780@gmail.com>
2025-10-17 19:28:12 +0700

    feat(backend): update featched data for list pages

--- backend/src/services/classesService.ts
+++ backend/src/services/classesService.ts
@@ -43,7 +43,11 @@ export const getClassesWithinPeriod = async (
           lte: endDate,
         },
       },
-      include: { instructor: true, customer: true },
+      include: {
+        instructor: true,
+        customer: true,
+        classAttendance: { include: { children: true } },
+      },
       orderBy: { dateTime: "asc" },
     });
 
