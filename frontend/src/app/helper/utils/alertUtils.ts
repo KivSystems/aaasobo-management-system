b7c3fe7f44ce9676191aee8abcdbbca12c851e6e
momori-nakano <momori.nakano@gmail.com>
2025-11-03 23:58:54 -0500

    refactor: remove unused customAlert helper

--- frontend/src/app/helper/utils/alertUtils.ts
+++ frontend/src/app/helper/utils/alertUtils.ts
@@ -1,27 +1,5 @@
 import Swal from "sweetalert2";
 
-export const customAlert: (settings: AlertOptions) => Promise<boolean> = (
-  settings: AlertOptions,
-) => {
-  const result = Swal.fire({
-    title: settings.title,
-    text: settings.text,
-    icon: settings.icon,
-    confirmButtonText: settings.confirmButtonText,
-    cancelButtonText: settings.cancelButtonText,
-    showCancelButton: settings.showCancelButton,
-    showConfirmButton: settings.showConfirmButton,
-    position: settings.position || "center",
-  }).then((result) => {
-    if (result.isConfirmed) {
-      return true;
-    } else {
-      return false;
-    }
-  });
-  return result;
-};
-
 export const confirmAlert: (text: string) => Promise<boolean> = (
   text: string,
 ) => {
