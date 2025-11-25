9bdcde2aee7859b42e2234aef4c8b5149fdb9a70
mtkksk1780 <mtkksk1780@gmail.com>
2025-11-26 00:53:55 +0700

    feat: remove route groups in admin folder

---
+++ frontend/src/app/admins/[id]/child-list/page.tsx
@@ -0,0 +1,38 @@
+import ListTable from "@/app/components/admins-dashboard/ListTable";
+import { getAllChildren } from "@/app/helper/api/adminsApi";
+import { authenticateUserSession } from "@/app/helper/auth/sessionUtils";
+import { getCookie } from "../../../../middleware";
+
+export default async function Page({ params }: { params: { id: string } }) {
+  // Authenticate user session
+  const adminId = params.id;
+  await authenticateUserSession("admin", adminId);
+
+  // Get the cookies from the request headers
+  const cookie = await getCookie();
+
+  // Define table configuration
+  const listType = "Child List";
+  const omitItems = [""]; // Omit the item from the table
+  const linkItems = ["ID"]; // Set the item to be a link
+  const replaceItems = ["Customer ID"]; // Replace the item with the value(e.g., ID -> 1,2,3...)
+  const linkUrls = [`/admins/${adminId}/customer-list/[Customer ID]`]; // Set the link URL
+  const userType = "admin"; // Set the user type for the registration form (It's not used in this page, but kept for consistency)
+  const isAddButton = false; // Enable the add button
+  const data = await getAllChildren(cookie); // Fetch all children data
+
+  return (
+    <div>
+      <ListTable
+        listType={listType}
+        fetchedData={data}
+        omitItems={omitItems}
+        linkItems={linkItems}
+        linkUrls={linkUrls}
+        replaceItems={replaceItems}
+        userType={userType}
+        isAddButton={isAddButton}
+      />
+    </div>
+  );
+}
---
+++ frontend/src/app/admins/[id]/class-list/[classId]/page.tsx
@@ -0,0 +1,56 @@
+import ClassDetails from "@/app/components/instructors-dashboard/class-schedule/classDetails/ClassDetails";
+import {
+  getSameDateClasses,
+  getInstructorIdByClassId,
+} from "@/app/helper/api/instructorsApi";
+import { authenticateUserSession } from "@/app/helper/auth/sessionUtils";
+import { getCookie } from "../../../../../middleware";
+
+const Page = async ({
+  params,
+}: {
+  params: { id: string; classId: string };
+}) => {
+  // Authenticate user session
+  const userSessionType = await authenticateUserSession("admin", params.id);
+  const adminId = parseInt(params.id);
+
+  // Get the cookies from the request headers
+  const cookie = await getCookie();
+
+  // Get the classId from the URL parameters
+  const classId = parseInt(params.classId);
+  if (isNaN(classId)) {
+    return <p>Class not found (invalid class id)</p>;
+  }
+
+  // Get the instructorId from the applicable class information
+  const response = await getInstructorIdByClassId(classId, cookie);
+
+  let instructorId: number;
+  if (response && "instructorId" in response) {
+    instructorId = response.instructorId;
+  } else {
+    return <p>Class not found (invalid instructor id)</p>;
+  }
+
+  const { selectedClassDetails, sameDateClasses } = await getSameDateClasses(
+    instructorId,
+    classId,
+    cookie,
+  );
+
+  return (
+    <ClassDetails
+      adminId={adminId}
+      instructorId={instructorId}
+      classId={classId}
+      userSessionType={userSessionType}
+      classDetails={selectedClassDetails}
+      classes={sameDateClasses}
+      previousPage="class-list"
+    />
+  );
+};
+
+export default Page;
