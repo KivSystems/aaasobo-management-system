c07b6c589a6a2fb584102a94c334ebb63097bad9
mtkksk1780 <mtkksk1780@gmail.com>
2026-01-06 00:54:08 +0700

    feat(backend): update prisma client setting

--- backend/prisma/prismaClient.ts
+++ backend/prisma/prismaClient.ts
@@ -1,15 +1,23 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient, Prisma } from "../generated/prisma/client";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.POSTGRES_PRISMA_URL,
-});
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export const prisma = new PrismaClient({
-  adapter,
-  transactionOptions: {
-    isolationLevel: Prisma.TransactionIsolationLevel.Serializable,
-    maxWait: 5000,
-    timeout: 60000,
-  },
-});
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter: new PrismaPg({
+      connectionString: process.env.POSTGRES_PRISMA_URL,
+    }),
+    transactionOptions: {
+      isolationLevel: Prisma.TransactionIsolationLevel.ReadCommitted,
+      maxWait: 5000,
+      timeout: 60000,
+    },
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
